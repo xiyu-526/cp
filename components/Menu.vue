20a0a85939b8059fef5ef47ae595ea39d31552a7
xiyu-526 <xiyu@ztgame.com>
2024-04-22 18:28:08 +0800

    修改样式

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="menu" :class="{'cur':curClass}">
-    <div class="logo"></div>
+  <div class="menu" :class="{ cur: curClass }">
+    <div class="logo" @click="backtop()"></div>
     <ul>
-      <li><nuxt-link :to="{path: '/'}">首页</nuxt-link></li>
+      <li><nuxt-link :to="{ path: '/' }">首页</nuxt-link></li>
       <li><a href="https://my.ztgame.com/main" target="_blank">账号管理</a></li>
       <li><a href="https://ipay.ztgame.com" target="_blank">支付中心</a></li>
       <li><a href="https://jrkf.ztgame.com/" target="_blank">客服中心</a></li>
-      <li><a href="https://jrkf.ztgame.com/index.php?g=front&m=index&a=article&aid=5365&term_name=%E5%BF%AB%E6%8D%B7%E6%9C%8D%E5%8A%A1" target="_blank">家长监护</a></li>
+      <li>
+        <a
+          href="https://jrkf.ztgame.com/index.php?g=front&m=index&a=article&aid=5365&term_name=%E5%BF%AB%E6%8D%B7%E6%9C%8D%E5%8A%A1"
+          target="_blank"
+          >家长监护</a
+        >
+      </li>
       <li><a href="https://hr.ztgame.com" target="_blank">诚聘英才</a></li>
     </ul>
     <div class="wb">
@@ -21,9 +27,13 @@
   </div>
 </template>
 <script setup>
-    const curClass = ref(false);
-    const keydown = function(){
-      curClass.value = !curClass.value;
-    }
+const emit = defineEmit(["backtop"]);
+const curClass = ref(false);
+const keydown = function () {
+  curClass.value = !curClass.value;
+};
+
+const backtop = () => {
+  emit("backtop");
+};
 </script>
-  
